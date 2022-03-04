a79b56164218e373cde9be566c9e0f610f4d1e42
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-03-03 22:02:46 -0500

    family class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("I have made changes");
         String s = "ASSIGNMENT";
         char arr[] = new char[s.length()];
         for (int i = 0; i < s.length(); i++) {
