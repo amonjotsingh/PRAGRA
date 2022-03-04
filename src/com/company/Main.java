a2d3cedca7fb60f7d57f9e5c846ae00273b068eb
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-03-03 22:07:15 -0500

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("this is trying to commit to master to use as merge");
         String s = "ASSIGNMENT";
         char arr[] = new char[s.length()];
         for (int i = 0; i < s.length(); i++) {
